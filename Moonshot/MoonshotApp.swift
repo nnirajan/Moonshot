7658052e0aefac73241f0a32a53a055d1dc76180
Nirajan Shrestha <nirajan.shrestha@ebpearls.com>
2022-12-22 16:29:04 +0545

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by ebpearls on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
